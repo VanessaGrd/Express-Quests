d187e6ca910545b2c260055ed34114dfc890ef35
VanessaGrd <suede210810@gmail.com>
2023-05-23 10:39:20 +0200

    Quest4bis

--- validators.js
+++ validators.js
@@ -35,21 +35,32 @@ const validateMovie = (req, res, next) => {
     }
   };
 
-  const validateUser = (req, res, next) => {
-    const { email } = req.body;
-    const errors = [];
-  
-    const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-  
-    if (!emailRegex.test(email)) {
-      errors.push({ field: 'email', message: 'Invalid email' });
-    }
-  
-  
-    if (errors.length) {
-      res.status(422).json({ validationErrors: errors });
-    } else {
-      next();
-    }
-  };
+  const { firstname, lastname, email, city, language } = req.body;
+  const errors = [];
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+  if (firstname == null) {
+    errors.push({ field: "title", message: "This field is required" });
+  }
+  if (lastname == null) {
+    errors.push({ field: "director", message: "This field is required" });
+  }
+  if (email == null) {
+    errors.push({ field: "year", message: "This field is required" });
+  }
+  if (!emailRegex.test(email)) {
+    errors.push({ field: "email", message: "Invalid email" });
+  }
+  if (city == null) {
+    errors.push({ field: "color", message: "This field is required" });
+  }
+  if (language == null) {
+    errors.push({ field: "duration", message: "This field is required" });
+  }
+  if (errors.length) {
+    res.status(422).json({ validationErrors: errors });
+  } else {
+    next();
+  }
+
  module.exports = {validateMovie, validateUser};
